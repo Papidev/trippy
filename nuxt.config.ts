79a7aad75ae39bff23eee0649782f1e1c6df9276
Luca <lzanzottera@gmail.com>
2023-02-11 23:54:45 +0100

    installed maplibre and first nuxt modules (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://unpkg.com/maplibre-gl@2.4.0/dist/maplibre-gl.css',
+        },
+      ],
+    },
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/apollo',
+    '@nuxtjs/strapi',
+    'nuxt-headlessui',
+  ],
   apollo: {
     clients: { default: { httpEndpoint: 'http://localhost:1337/graphql' } },
   },
+  strapi: {},
 });
